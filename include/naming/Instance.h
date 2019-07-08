98b587d9d6a64da7ea21f53ca979060704f5b16b
TTTTTAAAAAKKKKEEEENNNN <48080812+TTTTTAAAAAKKKKEEEENNNN@users.noreply.github.com>
2019-07-08 23:13:21 +0800

    + declaration

---
+++ include/naming/Instance.h
@@ -0,0 +1,62 @@
+#ifndef __INSTANCE_H_
+#define __INSTANCE_H_
+#include <map>
+#include "NacosString.h"
+
+class Instance
+{
+public:
+	/**
+	* unique id of this instance.
+	*/
+	String instanceId;
+
+	/**
+	* instance ip
+	*/
+	String ip;
+
+	/**
+	* instance port
+	*/
+	int port;
+
+	/**
+	* instance weight
+	*/
+	double weight = 1.0D;
+
+	/**
+	* instance health status
+	*/
+	bool healthy = true;
+
+	/**
+	* If instance is enabled to accept request
+	*/
+	bool enabled = true;
+
+	/**
+	* If instance is ephemeral
+	*
+	* @since 1.0.0
+	*/
+	bool ephemeral = true;
+
+	/**
+	* cluster information of instance
+	*/
+	String clusterName;
+
+	/**
+	* Service information of instance
+	*/
+	String serviceName;
+
+	/**
+	* user extended attributes
+	*/
+	std::map<String, String> metadata;
+};
+
+#endif
---
+++ include/naming/NamingProxy.h
@@ -0,0 +1,6 @@
+#ifndef __NAMING_PROXY_H_
+#define __NAMING_PROXY_H_
+
+
+
+#endif
