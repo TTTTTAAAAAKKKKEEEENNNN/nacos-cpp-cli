3ff14b3bd5e9674a0d94ce649bc2819d70a94c8d
liuhanyu <liuhanyu@chinalife.com.hk>
2019-07-24 17:51:24 +0800

    + valueOf, Java-style converter

--- include/NacosString.h
+++ include/NacosString.h
@@ -1,6 +1,7 @@
 #ifndef __NACOS_STRING_H_
 #define __NACOS_STRING_H_
 #include <string>
+#include <sstream>
 #define String std::string
 #define NULLSTR NacosString::nullstr
 #define isNull NacosString::isNullStr
@@ -10,6 +11,19 @@ class NacosString
 public:
 	static const String nullstr;
 	static bool isNullStr(const String &str);
+	template <typename T>
+	static String valueOf(T val)
+	{
+		std::ostringstream os;
+		if (os << val)
+		{
+			return String(os.str().c_str());
+		}
+
+		return NULLSTR;
+	}
+	static const String TRUE;
+	static const String FALSE;
 };
 
 #endif
--- src/NacosString.cpp
+++ src/NacosString.cpp
@@ -1,5 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+#include <sstream>
 #include "NacosString.h"
+
 const String NacosString::nullstr = "";
+const String NacosString::TRUE = "true";
+const String NacosString::FALSE = "false";
 
 //Returns true if str refers to nullstr
 bool NacosString::isNullStr(const String &str)
